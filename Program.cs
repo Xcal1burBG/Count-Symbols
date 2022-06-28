b7a283cf75aa32c604ca4f490718a40384ba9dc1
Xcal1burBG <99122623+Xcal1burBG@users.noreply.github.com>
2022-06-28 21:52:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedDictionary<char, int> myDictionary = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!myDictionary.ContainsKey(input[i]))
+                {
+                    myDictionary.Add(input[i], 1);
+                }
+
+                else
+                {
+                    myDictionary[input[i]] += 1;
+                }
+
+            }
+
+           
+
+            foreach (var symbol in myDictionary)
+            {
+                Console.WriteLine($"{symbol}: {myDictionary[symbol.Key]} time/s ");
+            }
+        }
+    }
+}
